7b38622cf619c08f5203e7f9e6aacf6efd8d9fab
JordanPonder <JordanPonder@protonmail.ch>
2020-12-13 03:34:00 -0500

    framework

--- TheSpeed-FE/src/app/Profile/profile-general/profile-general.component.ts
+++ TheSpeed-FE/src/app/Profile/profile-general/profile-general.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileGeneralComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+ 
+  }
 
   ngOnInit(): void {
   }
