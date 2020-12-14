14e90e5828b82174e29c1627b1da6708301f9cb9
Alex Wolfe <troy.hicks1@yahoo.com>
2020-12-14 15:30:48 -0600

    App wide cleanup infra-changes

---
+++ TheSpeed-FE/src/app/shared/constants.ts
@@ -0,0 +1,40 @@
+
+// App URLs
+
+export const APP_URL = 'https://localhost:4200';
+
+
+// Icon Urls ----- TODO to later be saved as a file in repository
+
+export const SPEED_LOGO = {
+    BIG: '',
+    NAV: 'assets/img/logo.png',
+    ICON: 'assets/img/TheSpeedCropped2.jpg', // TODO Needs To be Updated
+    NAME: 'assets/img/The Speed.png',
+    TEST: 'artwork/logo.png',
+};
+
+export const NAV_ICONS = {
+
+    NAV_HOME: 'https://img.icons8.com/ios-filled/25/000000/home.png',
+    NAV_CREATE: 'https://img.icons8.com/ios-filled/25/000000/create-new.png',
+    NAV_EVENTS: 'https://img.icons8.com/ios-filled/25/000000/f1-race-car-top-veiw.png',
+    NAV_PROFILE: 'https://img.icons8.com/ios-filled/25/000000/user.png'
+};
+
+export const POST_ICONS = {
+    POST_LIKE: 'https://img.icons8.com/ios-filled/15/000000/like.png',
+    POST_COMMENT: 'https://img.icons8.com/ios-filled/15/000000/comment-discussion.png',
+    POST_SHARE: 'https://img.icons8.com/ios-filled/15/000000/link--v1.png'
+};
+
+export const SOCIAL_ICONS = {
+
+    INSTA_LOGO: '',
+    WEB_LOGO: '',
+    TWIT_LOGO: '',
+    FACE_LOGO: ''
+};
+
+export const BASE_AVI_LOGO = 'https://bootdey.com/img/Content/avatar/avatar7.png';
+export const BASE_POST_PIC = '';
