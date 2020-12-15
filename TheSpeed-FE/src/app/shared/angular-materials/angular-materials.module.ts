ba3b476785b48046e6e4b19538637bf10a51bae6
JordanPonder <JordanPonder@protonmail.ch>
2020-12-15 01:14:30 -0500

    feel free to delete this branch

--- TheSpeed-FE/src/app/shared/angular-materials/angular-materials.module.ts
+++ TheSpeed-FE/src/app/shared/angular-materials/angular-materials.module.ts
@@ -1,13 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  MatButtonModule, MatToolbarModule, MatCheckboxModule, MatMenuModule, MatIconModule,
-  MatCardModule, MatInputModule, MatSidenavModule, MatDividerModule, MatListModule, MatFormFieldModule,
-  MatSelectModule, MatTabsModule, MatGridListModule, MatButtonToggleModule, MatSlideToggleModule,
-  MatRadioModule, MatTableModule, MatPaginatorModule, MatProgressSpinnerModule, MatButtonToggleGroup,
-  MatSnackBarModule, MatSlideToggle, MatButtonToggle, MatButton, MatRadioGroup, MatSpinner, MatDialogModule,
-  MatTooltipModule, MatPaginator, MatGridTile, MatGridList, MatTab, MatRadioButton
-} from '@angular/material';
+import { MatButtonModule, MatButton } from '@angular/material/button';
+import { MatButtonToggleModule, MatButtonToggleGroup, MatButtonToggle } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule, MatGridTile, MatGridList } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule, MatPaginator } from '@angular/material/paginator';
+import { MatProgressSpinnerModule, MatSpinner } from '@angular/material/progress-spinner';
+import { MatRadioModule, MatRadioGroup, MatRadioButton } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSlideToggleModule, MatSlideToggle } from '@angular/material/slide-toggle';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule, MatTab } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatStepperModule } from '@angular/material/stepper';
 
 @NgModule({
